3de461678aba837bcadbc92c06a52db0191357ba
nbayoungnate4kt <Nathanael134@hotmail.com>
2019-05-24 14:50:03 -0400

    finish book site

--- client/main.js
+++ client/main.js
@@ -4,78 +4,105 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../lib/collection.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+
+Template.mainBody.helpers({
+  profAll(){
+    return userDB.find({}, {sort:{Likes: -1}});
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.mainBody.events({
+  'click .js-thumbsup'(event, instance) {
+    var likes = userDB.findOne({'_id':this._id}).Likes;
+    var dislikes = userDB.findOne({'_id':this._id}).Dislikes;
+    if(!likes){
+    likes=0;
+  }
+  likes++;
+  var ratio = (likes/(likes+dislikes)) *100;
+  userDB.update({"_id":this._id}, {$set:{'Likes':likes, 'LikeRatio':ratio}});
+  },
+
+  'click .js-thumbsdown'(event, instance) {
+    var likes = userDB.findOne({'_id':this._id}).Likes;
+    var dislikes = userDB.findOne({'_id':this._id}).Dislikes;
+    if(!dislikes){
+    dislikes=0;
+  }
+  dislikes++;
+  var ratio = (dislikes/(likes+dislikes)) *100;
+  userDB.update({"_id":this._id}, {$set:{'Dislikes':dislikes, 'DislikeRatio':ratio}});
+  },
+
+  'click .js-showbook'(event, instance) {
+    var modalname = '#viewinfo' + this._id;
+  $(modalname).modal('show');
+    var views = userDB.findOne({'_id':this._id}).Views;
+    if(!views){
+    views=0;
+  }
+  views++;
+  userDB.update({"_id":this._id}, {$set:{'Views':views}});
+  },
+
+  'click .js-delete'(event, instance) {
+  var profID = this._id;
+  $("#" + profID).fadeOut("slow","swing",function(){
+    userDB.remove({_id:profID});  
+  });
+  $('#editbook' + profID).modal('hide');
+  },
+
+  'click .js-addbook'(event, instance) {
+    var title = $('#adduser input[id="bookTitle"]').val();
+    var a_name = $('#adduser input[id="authorName"]').val();
+    var image = $('#adduser input[id="bookCover"]').val();
+    var desc = $('#adduser textarea[id="description"]').val();
+    if(title==undefined||title==""){
+      title = "UNKNOWN";
+    }
+    if(a_name==undefined||a_name==""){
+      a_name = "UNKNOWN";
+    }
+    if(image==undefined||image==""){
+      image = "book.jpg";
+    }
+    if(desc==undefined||desc==""){
+      desc = "UNKNOWN";
+    }
+    $('#adduser input[id="bookTitle"]').val('');
+    $('#adduser input[id="authorName"]').val('');
+    $('#adduser input[id="bookCover"]').val('');
+    $('#adduser textarea[id="description"]').val('');
+    $('#adduser').modal('hide');
+  userDB.insert({'bookTitle':title, 'authorName':a_name, 'bookCover':image, 'desc':desc, 'Likes':0, 'Dislikes':0, 'LikeRatio':0, 'DislikeRatio':0, 'Views':0});
+  },
+
+  'click .js-savebook'(event, instance) {
+    var profID = this._id;
+    var modalname = '#editbook' + this._id;
+    var title = $(modalname + ' input[id="bookTitle"]').val();
+    var a_name = $(modalname + ' input[id="authorName"]').val();
+    var image = $(modalname + ' input[id="bookCover"]').val();
+    var desc = $(modalname + ' textarea[id="description"]').val();
+    if(title==""){
+      title = this.bookTitle;
+    }
+    if(a_name==""){
+      a_name = this.authorName;
+    }
+    if(image==""){
+      image = this.bookCover;
+    }
+    if(desc==""){
+      desc = this.desc;
+    }
+    $(modalname + ' input[id="bookTitle"]').val('');
+    $(modalname + ' input[id="authorName"]').val('');
+    $(modalname + ' input[id="bookCover"]').val('');
+    $(modalname + ' textarea[id="description"]').val('');
+    $(modalname).modal('hide');
+  userDB.update({"_id":profID}, {$set:{'bookTitle':title, 'authorName':a_name, 'bookCover':image, 'desc':desc}});
   },
 });
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-
-Template.mainBody.helpers({//template helpers put data into the template
-	myJumbo(){
-		return userDB.find({});
-	}
-});
-
-Template.mainBody.events({//template events define and customize user interactions(events) within the template
-	'click .js-Thumbsup'(event, instance) {
- 		console.log("You clicked thumbsup");
- 		var profID = this._id;
- 		var numlikes = userDB.findOne({_id: profID}).Thumbsup;
- 		if(!numlikes){
- 			numlikes = 0;
- 		}
- 		numlikes= numlikes+1;
- 		userDB.update({_id:profID}, {$set:{'Thumbsup':numlikes}});
-  	},
-  	'click .js-Thumbsdown'(event, instance) {
-  		console.log("You clicked thumbsup");
-  		var profID = this._id;
-  		var numlikes = userDB.findOne({_id: profID}).Thumbsdown;
- 		if(!numlikes){
- 			numlikes = 0;
- 		}
- 		numlikes= numlikes+1;
- 		userDB.update({_id:profID}, {$set:{'Thumbsdown':numlikes}});
-  	},
-  	'click .js-delete'(event,instance) {
-  		var profID = this._id;
-  		$("#" + profID).fadeOut("slow","swing",function(){
-  			userDB.remove({_id:profID}); 	
-  		});
-  	},
-  	'click .profEdit'(event,instance){
-  		$('#modaledit').modal('show');
-  		console.log("------")
-  	}
-
-});
-
-Template.addbook.events({
-	'click .js-save'(event, instance) {
-		var fname = $('#exampleModal input[name="firstName"]').val()
-		var lname = $('#exampleModal input[name="lastName"]').val()
-		var img = $('#exampleModal input[name="Image"]').val()
-		if(img ==""){
-			img="book1.jpg"
-		}
-		console.log("The name is",fname,lname);
-		$('#exampleModal input[name="firstName"]').val('')
-		$('#exampleModal input[name="lastName"]').val('')
-		$('#exampleModal input[name="Image"]').val('')
-		$('#exampleModal').modal('hide');
-		userDB.insert({'firstName':fname,'lastName':lname, 'Image':img});
-  		
-  },
-}); 
